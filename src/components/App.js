1ac4c6f1307db95c0f858a2b2b546bdf382c5673
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-04-11 12:29:01 +0000

    remove p tag inside li tag

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const App = () => {
-  const realtiveList = ["Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace"];
+  const realtives = ["Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace"];
   return (
     <div id="main">
                {/* Do not remove the main div */}
       <ol key={relativeList}>
         {
-          realtiveList.length>0 && realtiveList.map((val,ind)=>(
-            <li key={ `relativeListItem ${ind+1}`}>
+          realtives.length>0 && realtives.map((val,ind)=>(
+            <li key={ `relativeListItem${ind+1}`}>
               {val}
             </li>
           ))
