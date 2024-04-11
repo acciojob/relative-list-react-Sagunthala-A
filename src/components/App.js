eefed06f63ecac214122cc2c13ce2d5047c886d5
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-04-11 12:12:00 +0000

    done  task

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const App = () => {
+  const realtiveList = ["Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace"];
   return (
     <div id="main">
                {/* Do not remove the main div */}
+      <ol key={relativeList}>
+        {
+          realtiveList.map((val,ind)=>(
+            <li key={`relativeListItem ${ind+1}`}>
+              <p>{val}</p>
+            </li>
+          ))
+        }
+      </ol>
     </div>
   )
 }
